55a7d18dd991e802ae39ce5ae561c8bd149e64e8
Timo Strolz <TStrolz@cl06.ch>
2019-06-05 23:03:58 +0200

    hinzufügen angefangen & zustand ändern *solllte* funtionieren

---
+++ e-Bibliothek/e-Bibliothek/HinzufügenForm.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace e_Bibliothek
+{
+    partial class HinzufügenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuch = new System.Windows.Forms.Button();
+            this.btnKS = new System.Windows.Forms.Button();
+            this.btnZeitung = new System.Windows.Forms.Button();
+            this.btnDVD = new System.Windows.Forms.Button();
+            this.btnBHinzufügen = new System.Windows.Forms.Button();
+            this.textBoxBAutor = new System.Windows.Forms.TextBox();
+            this.textBoxBS = new System.Windows.Forms.TextBox();
+            this.textBoxBTitel = new System.Windows.Forms.TextBox();
+            this.textBoxBISNN = new System.Windows.Forms.TextBox();
+            this.textBoxBArt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panelBuch = new System.Windows.Forms.Panel();
+            this.panelZeitung = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBBAck = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelBuch.SuspendLayout();
+            this.panelZeitung.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBuch
+            // 
+            this.btnBuch.Location = new System.Drawing.Point(13, 13);
+            this.btnBuch.Name = "btnBuch";
+            this.btnBuch.Size = new System.Drawing.Size(79, 30);
+            this.btnBuch.TabIndex = 0;
+            this.btnBuch.Text = "Buch hinzufügen";
+            this.btnBuch.UseVisualStyleBackColor = true;
+            this.btnBuch.Click += new System.EventHandler(this.btnBuch_Click);
+            // 
+            // btnKS
+            // 
+            this.btnKS.Location = new System.Drawing.Point(13, 49);
+            this.btnKS.Name = "btnKS";
+            this.btnKS.Size = new System.Drawing.Size(79, 30);
+            this.btnKS.TabIndex = 1;
+            this.btnKS.Text = "Konsolenspiel hinzufügen";
+            this.btnKS.UseVisualStyleBackColor = true;
+            this.btnKS.Click += new System.EventHandler(this.btnKS_Click);
+            // 
+            // btnZeitung
+            // 
+            this.btnZeitung.Location = new System.Drawing.Point(13, 85);
+            this.btnZeitung.Name = "btnZeitung";
+            this.btnZeitung.Size = new System.Drawing.Size(79, 30);
+            this.btnZeitung.TabIndex = 2;
+            this.btnZeitung.Text = "Zeitung hinzufügen";
+            this.btnZeitung.UseVisualStyleBackColor = true;
+            this.btnZeitung.Click += new System.EventHandler(this.btnZeitung_Click);
+            // 
+            // btnDVD
+            // 
+            this.btnDVD.Location = new System.Drawing.Point(12, 121);
+            this.btnDVD.Name = "btnDVD";
+            this.btnDVD.Size = new System.Drawing.Size(79, 30);
+            this.btnDVD.TabIndex = 3;
+            this.btnDVD.Text = "DVD hinzufügen";
+            this.btnDVD.UseVisualStyleBackColor = true;
+            this.btnDVD.Click += new System.EventHandler(this.btnDVD_Click);
+            // 
+            // btnBHinzufügen
+            // 
+            this.btnBHinzufügen.Location = new System.Drawing.Point(4, 395);
+            this.btnBHinzufügen.Name = "btnBHinzufügen";
+            this.btnBHinzufügen.Size = new System.Drawing.Size(105, 33);
+            this.btnBHinzufügen.TabIndex = 0;
+            this.btnBHinzufügen.Text = "Hinzufügen";
+            this.btnBHinzufügen.UseVisualStyleBackColor = true;
+            this.btnBHinzufügen.Click += new System.EventHandler(this.btnBHinzufügen_Click);
+            // 
+            // textBoxBAutor
+            // 
+            this.textBoxBAutor.Location = new System.Drawing.Point(16, 36);
+            this.textBoxBAutor.Name = "textBoxBAutor";
+            this.textBoxBAutor.Size = new System.Drawing.Size(100, 26);
+            this.textBoxBAutor.TabIndex = 1;
+            // 
+            // textBoxBS
+            // 
+            this.textBoxBS.Location = new System.Drawing.Point(13, 332);
+            this.textBoxBS.Name = "textBoxBS";
+            this.textBoxBS.Size = new System.Drawing.Size(100, 26);
+            this.textBoxBS.TabIndex = 2;
+            // 
+            // textBoxBTitel
+            // 
+            this.textBoxBTitel.Location = new System.Drawing.Point(17, 105);
+            this.textBoxBTitel.Name = "textBoxBTitel";
+            this.textBoxBTitel.Size = new System.Drawing.Size(100, 26);
+            this.textBoxBTitel.TabIndex = 3;
+            // 
+            // textBoxBISNN
+            // 
+            this.textBoxBISNN.Location = new System.Drawing.Point(16, 254);
+            this.textBoxBISNN.Name = "textBoxBISNN";
+            this.textBoxBISNN.Size = new System.Drawing.Size(100, 26);
+            this.textBoxBISNN.TabIndex = 5;
+            // 
+            // textBoxBArt
+            // 
+            this.textBoxBArt.Location = new System.Drawing.Point(17, 177);
+            this.textBoxBArt.Name = "textBoxBArt";
+            this.textBoxBArt.Size = new System.Drawing.Size(100, 26);
+            this.textBoxBArt.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Autor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "art";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "seiten";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 231);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "isbnnummer";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Titel";
+            // 
+            // panelBuch
+            // 
+            this.panelBuch.Controls.Add(this.panelZeitung);
+            this.panelBuch.Controls.Add(this.btnBBAck);
+            this.panelBuch.Controls.Add(this.label8);
+            this.panelBuch.Controls.Add(this.label7);
+            this.panelBuch.Controls.Add(this.label6);
+            this.panelBuch.Controls.Add(this.label5);
+            this.panelBuch.Controls.Add(this.label1);
+            this.panelBuch.Controls.Add(this.textBoxBArt);
+            this.panelBuch.Controls.Add(this.textBoxBISNN);
+            this.panelBuch.Controls.Add(this.textBoxBTitel);
+            this.panelBuch.Controls.Add(this.textBoxBS);
+            this.panelBuch.Controls.Add(this.textBoxBAutor);
+            this.panelBuch.Controls.Add(this.btnBHinzufügen);
+            this.panelBuch.Location = new System.Drawing.Point(0, 0);
+            this.panelBuch.Name = "panelBuch";
+            this.panelBuch.Size = new System.Drawing.Size(801, 451);
+            this.panelBuch.TabIndex = 4;
+            // 
+            // panelZeitung
+            // 
+            this.panelZeitung.Controls.Add(this.textBox1);
+            this.panelZeitung.Controls.Add(this.label2);
+            this.panelZeitung.Location = new System.Drawing.Point(4, 3);
+            this.panelZeitung.Name = "panelZeitung";
+            this.panelZeitung.Size = new System.Drawing.Size(797, 448);
+            this.panelZeitung.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // btnBBAck
+            // 
+            this.btnBBAck.Location = new System.Drawing.Point(154, 395);
+            this.btnBBAck.Name = "btnBBAck";
+            this.btnBBAck.Size = new System.Drawing.Size(103, 33);
+            this.btnBBAck.TabIndex = 15;
+            this.btnBBAck.Text = "Zurück";
+            this.btnBBAck.UseVisualStyleBackColor = true;
+            this.btnBBAck.Click += new System.EventHandler(this.btnBBAck_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // HinzufügenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelBuch);
+            this.Controls.Add(this.btnDVD);
+            this.Controls.Add(this.btnZeitung);
+            this.Controls.Add(this.btnKS);
+            this.Controls.Add(this.btnBuch);
+            this.Name = "HinzufügenForm";
+            this.Text = "HinzufügenForm";
+            this.panelBuch.ResumeLayout(false);
+            this.panelBuch.PerformLayout();
+            this.panelZeitung.ResumeLayout(false);
+            this.panelZeitung.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuch;
+        private System.Windows.Forms.Button btnKS;
+        private System.Windows.Forms.Button btnZeitung;
+        private System.Windows.Forms.Button btnDVD;
+        private System.Windows.Forms.Button btnBHinzufügen;
+        private System.Windows.Forms.TextBox textBoxBAutor;
+        private System.Windows.Forms.TextBox textBoxBS;
+        private System.Windows.Forms.TextBox textBoxBTitel;
+        private System.Windows.Forms.TextBox textBoxBISNN;
+        private System.Windows.Forms.TextBox textBoxBArt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panelBuch;
+        private System.Windows.Forms.Button btnBBAck;
+        private System.Windows.Forms.Panel panelZeitung;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ e-Bibliothek/e-Bibliothek/HinzufügenForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_Bibliothek
+{
+    public partial class HinzufügenForm : Form
+    {
+        public HinzufügenForm()
+        {
+            InitializeComponent();
+            panelBuch.Hide();
+        }
+
+
+        private void btnBuch_Click(object sender, EventArgs e)
+        {
+            panelBuch.Show();
+        }
+
+        private void btnKS_Click(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void btnZeitung_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDVD_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBHinzufügen_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            string sql = null;
+            connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\timos\Source\Repos\eBibliothek2\e-Bibliothek\e-Bibliothek\Database1.mdf;Integrated Security=True";
+            // Create a new Benutzer object.
+            //insert into 
+            sql = String.Format("INSERT INTO Bücher (autor, titel, isbnnummer, art, seiten, verfügbarkeit) VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '0'); ", textBoxBAutor.Text, textBoxBTitel.Text, textBoxBISNN.Text, textBoxBArt.Text, textBoxBS.Text);
+            connection = new SqlConnection(connetionString);
+
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+
+                command.ExecuteNonQuery();
+                //command.Dispose();
+                connection.Close();
+            }
+            catch (Exception z)
+            {
+                MessageBox.Show("Can not open connection ! " + z);
+            }
+        }
+
+        
+
+        private void btnBBAck_Click(object sender, EventArgs e)
+        {
+            panelBuch.Hide();
+        }
+
+
+    }
+}
